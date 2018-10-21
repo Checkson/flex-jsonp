2603c272015100fad77266be890acce043d52d55
Checkson <952052437@qq.com>
2018-10-21 11:13:47 +0800

    ver.0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # flex-jsonp
-A Flex JSONP Library
+Support Promise, Compatible ie9+
+
+## demo
+
+## install
+```
+  npm i flex-jsonp --save
+```
+
+## usage
+```javascript   
+flexJsonp({
+  url: 'http://suggestion.baidu.com/su',
+  params: {
+    wd: keywords,
+    p: 3,
+    t: new Date().getTime()
+  },
+  callbackParam: 'cb'
+}).then(function (res) {
+  console.log(res);
+}).catch(function (err) {
+  console.log(err);
+});
+```
+
+## parameters
+names | types | default | explain
+:---:|:---:|:---:|:---:
+url | string | '' | The url requested by JSONP
+params | object | {} | Request parameters
+timeout | number | 0 | Request timeout setting
+callbackParam | string | 'cb' | JSONP callback function parameter name
+callback | string | 'calback'+${seed} |  JSONP callback function name
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,135 @@
+/**
+ * Copyright (c) 2018-10-20 Checkson.
+ * Licensed under the MIT License (MIT).
+ * Github:https://github.com/Checkson/flex-jsonp
+ */
+
+;(function () {
+
+  'use strict';
+
+  // Dependent window environment.
+  if (typeof window === 'undefined') {
+    throw new Error('Flex jsonp needs to run in the window environment!');
+    return;
+  };
+
+  // Jsonp callback function seed.
+  var seed = 0;
+  // Judge if support promise.
+  var isSupport = typeof Promise === 'undefined' ? false : true;
+  // Choose encode type
+  var enc = encodeURIComponent;
+
+  // Clean up function.
+  var cleanup = function (script, cb, timer) {
+    // Remove the script tag.
+    if (script && script.parentNode) {
+      script.parentNode.removeChild(script);
+    }
+    // Remove jsonp callback function.
+    try {
+      delete window[cb];
+    } catch (e) {
+      window[cb] = null;
+    }
+    // Remove jsonp timeout listener
+    if (timer) {
+      clearTimeout(timer);
+    }
+  }
+
+  // Create script and add src
+  var createScript = function (url) {
+    var script = document.createElement('script'),
+        oHead = document.head || document.getElementsByTagName('head')[0];
+    script.src = url;
+    oHead.appendChild(script);
+    return script;
+  }
+
+  // Build url params
+  var buildUrlParams = function (url, params) {
+    var arr = [];
+    for (var key in params) {
+      arr.push(key + '=' + enc(params[key]));
+    }
+    var char = url.indexOf('?') > -1 ? '&' : '?';
+    return url + char + arr.join('&');
+  }
+
+  // Jsonp
+  var flexJsonp = function (options) {
+
+    var _this = this,
+        settings = options || {},
+        url = settings.url || '',
+        timeout = settings.timeout || 0,
+        params = settings.params || {},
+        callbackParam = settings.callbackParam || 'cb',
+        callback = settings.callback || 'callback'+seed++;
+
+    params[callbackParam] = callback;
+
+    var script = createScript(buildUrlParams(url, params)),
+        timer;
+
+    if (isSupport) {
+      return new Promise(function (resolve, reject) {
+          // Timeout
+          if (timeout) {
+            timer = setTimeout(function () {
+              cleanup(script, callback, timer);
+              reject(new Error('Timeout'))
+            }, timeout);
+          }
+          // Process the returned data
+          window[callback] = function (data) {
+            cleanup(script, callback, timer);
+            resolve(data);
+          }
+      });
+    } else {
+      var MyPromise = function () {
+        // Timeout
+        if (timeout) {
+          timer = setTimeout(function () {
+            cleanup(script, callback, timer);
+          }, timeout);
+        }
+        // Process the returned data
+        this.then = function (fn) {
+          window[callback] = function (data) {
+            cleanup(script, callback, timer);
+            fn && fn.call(_this, data);
+          }
+          return this;
+        };
+        // Catch timeout error
+        this.catch = function (fn) {
+          if (timer && timeout) {
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+              cleanup(script, callback, timer);
+              fn && fn.call(_this, new Error('Timeout'));
+            }, timeout);
+          }
+          return this;
+        }
+      }
+      return new MyPromise();
+    }
+  }
+
+  if (typeof module !== 'undefined' && module.exports) {
+    flexJsonp.default = flexJsonp;
+    module.exports = flexJsonp;
+  } else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    define('flexJsonp', [], function () {
+      return flexJsonp;
+    });
+  } else {
+    window.flexJsonp = flexJsonp;
+  }
+
+})();
